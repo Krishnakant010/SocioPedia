7d9d9b3141dd7ec1ce767218c8e36c952d1faae8
Krishnakant010 <krishnakantgangurde0109@gmail.com>
2023-07-08 15:59:58 +0530

    Hosting

--- client/src/components/UserImage.jsx
+++ client/src/components/UserImage.jsx
@@ -8,7 +8,7 @@ const UserImage = ({ image, size = "60px" }) => {
         width={size}
         height={size}
         alt="user"
-        src={`http://localhost:3001/assets/${image}`}
+        src={`https://sociopedia-vm7f.onrender.com/assets/${image}`}
       />
     </Box>
   );
